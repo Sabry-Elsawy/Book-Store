19103834bab718499166bbdf195825c75f5e8326
Sabry Elsawy <elsawysabry16@gmail.com>
2025-02-06 22:00:40 +0200

    update navbar

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  constructor(private elementRef: ElementRef) {}
+isOpen:boolean=false;
+isScrolled: boolean = false; // حالة التمرير
+toggleNavbar(){
+  this.isOpen=!this.isOpen;
 }
+  // الاستماع إلى حدث النقر في الصفحة
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    // التحقق مما إذا كان النقر خارج القائمة
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isOpen = false; // إغلاق القائمة
+    }
+  }
+
+
+    // الاستماع إلى حدث التمرير
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+      // التحقق من موضع التمرير
+      if (window.scrollY > 47) { // يمكنك تغيير القيمة 50 حسب الحاجة
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = false;
+      }
+    }
+}
+
